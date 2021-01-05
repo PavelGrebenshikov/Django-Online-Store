bb8f3b7cc26abf8886eda689165605ecf0b19386
Paul <pavgrebensh@mail.ru>
2021-01-05 11:16:04 +0300

    Edit database on sqlite

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'WebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Shop',
-        'USER': 'postgres',
-        'PASSWORD': 'hesoyam99',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -130,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
