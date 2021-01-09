3637d836590d07d061cf99984031f7607e91d08a
Paul <pavgrebensh@mail.ru>
2021-01-09 18:11:00 +0300

    Update fields admin.py

--- WebApp/user/admin.py
+++ WebApp/user/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Profile
 # Register your models here.
 
-admin.site.register(Profile)
+
+class ProfileUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'phone', 'birth_date', 'reg_date']
+    list_display_links = ['id', 'user', 'birth_date', 'reg_date']
+    search_fields = ['id', 'user', 'birth_date', 'reg_date']
+
+
+admin.site.register(Profile, ProfileUserAdmin)
